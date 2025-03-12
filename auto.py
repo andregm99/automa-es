e8fe0df704da9d9196eaa8fafe123aa464949baa
André Gomes <100249399+andregm99@users.noreply.github.com>
2025-03-12 20:07:53 -0300

    Update auto.py
    adicionando comentários

--- auto.py
+++ auto.py
@@ -9,7 +9,7 @@ navegador = webdriver.Chrome()
 # Acessar o site 
 navegador.get('https://ri.magazineluiza.com.br/ShowCanal/Planilha-de-Resultado?=CHN0/Z4bUSgrS8IkQeL+Wg==&linguagem=pt')
 
-# Localiza e clica no botão de "Aceitar cookies" (no site da Wikipedia)
+# Localiza e clica no botão de "Aceitar cookies" (no site da magazine luiza)
 accept_button = navegador.find_element(By.XPATH, '//*[@id="6BKAw9gH52TSXKs4jdvWkw=="]')  # XPath correto
 cookie= navegador.find_element(By.LINK_TEXT, 'OK')
 cookie.click()
